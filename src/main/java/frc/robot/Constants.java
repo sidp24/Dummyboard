3fb2864276cc75130028d81cd4072e1d57cd5b1a
sidp24 <56561908+sidp24@users.noreply.github.com>
2022-04-17 01:18:44 -0400

    finished container + buttons

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,24 +5,32 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
 
-    //variables for CIM Motor
+    // variables for CIM Motor
     public static final int CIM_ID = 0;
     public static final int CIM_SPEED = 0;
 
-    //variables for NEO Motor
+    // variables for NEO Motor
     public static final int NEO_ID = 0;
     public static final int NEO_SPEED = 0;
-    
-    //variables fpr Falcon Motor
+
+    // variables fpr Falcon Motor
     public static final int FALCO_ID = 0;
     public static final int FALCON_SPEED = 0;
+
+    // controllers
+    public static final int XBOX_CTRL_PORT = 0;
+    public static final int LOGIJOY_PORT = 0;
 }
